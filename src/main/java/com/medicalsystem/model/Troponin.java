f53a4d926a8c01f3a3a94de36baf82c7e2de1846
sienia0@gmail.com <sienia0@gmail.com>
2017-05-22 21:19:31 +0200

    Add Troponin entity

---
+++ src/main/java/com/medicalsystem/model/Troponin.java
@@ -0,0 +1,42 @@
+package com.medicalsystem.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "troponiny")
+public class Troponin {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_troponiny")
+    @Getter @Setter
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_przyjecia", nullable = false)
+    @Getter @Setter
+    private Admission admission;
+
+    @Column(name = "tnt", nullable = false)
+    @Getter @Setter
+    private float tnt;
+
+    @Column(name = "tnt_ultra", nullable = false)
+    @Getter @Setter
+    private float tnlUltra;
+
+    @Column(name = "tnl", nullable = false)
+    @Getter @Setter
+    private float tnl;
+
+    @Column(name = "tnt_doba", nullable = false)
+    @Getter @Setter
+    private float tntDay;
+
+    @Column(name = "tnl_doba", nullable = false)
+    @Getter @Setter
+    private float tnlDay;
+
+}
