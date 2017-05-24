fe0c58044acca01fa15cb25f8643c441027738d2
tradwan <tomasz.x.radwan@gmail.com>
2017-05-24 21:37:22 +0200

    Move component files to module folders

--- frontend/src/main/frontend/src/app/patient-form/patient-form.module.ts
+++ frontend/src/main/frontend/src/app/patient-form/patient-form.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PatientFormComponent } from './patient-form/patient-form.component';
+import { PatientFormComponent } from './patient-form.component';
 
 @NgModule({
   imports: [
