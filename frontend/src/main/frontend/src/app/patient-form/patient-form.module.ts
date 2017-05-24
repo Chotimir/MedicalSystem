d7fcf52fbad7ddff48fe176b5e0b6b1952d6ea91
tradwan <tomasz.x.radwan@gmail.com>
2017-05-24 20:32:18 +0200

    Add generated modules structure

---
+++ frontend/src/main/frontend/src/app/patient-form/patient-form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PatientFormComponent } from './patient-form/patient-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PatientFormComponent]
+})
+export class PatientFormModule { }
