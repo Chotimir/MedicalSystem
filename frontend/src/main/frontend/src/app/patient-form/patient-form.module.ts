2c8ce61169748a6657bbfe91c4faa60e0c559203
tradwan <tomasz.x.radwan@gmail.com>
2017-05-24 22:42:52 +0200

    Add ngx-translation to project

--- frontend/src/main/frontend/src/app/patient-form/patient-form.module.ts
+++ frontend/src/main/frontend/src/app/patient-form/patient-form.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientFormComponent } from './patient-form.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forChild()
   ],
   declarations: [PatientFormComponent]
 })
