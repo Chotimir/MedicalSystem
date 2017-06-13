733d83edb438f7d63c1a80307d18a22e493987ce
Jakub Piekarz <jpiekarz12@outlook.com>
2017-06-13 13:34:41 +0200

    Change diseases dictionary after Kamil's changes

--- backend/src/main/java/com/medicalsystem/DataLoader.java
+++ backend/src/main/java/com/medicalsystem/DataLoader.java
@@ -108,47 +108,47 @@ public class DataLoader implements ApplicationRunner {
 
 
         // opis_choroby_s
-        // HT
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(1, diseases[0], "nie"));
+		//wstrzas
+		diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(1, diseases[0], "nie"));
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(2, diseases[0], "tak"));
-        // CAD
+        // HT
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(3, diseases[1], "nie"));
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(4, diseases[1], "tak"));
-        // CAD wysokiego ryzyka
+        // CAD
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(5, diseases[2], "nie"));
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(6, diseases[2], "tak"));
-        // MI/ACS
+        // CAD wysokiego ryzyka
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(7, diseases[3], "nie"));
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(8, diseases[3], "tak"));
+        // MI/ACS
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(9, diseases[4], "nie"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(10, diseases[4], "tak"));
         // Stenoza aortalna
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(9, diseases[4], "brak"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(10, diseases[4], "łagodna"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(11, diseases[4], "umiarkowana"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(12, diseases[4], "ciężka"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(13, diseases[4], "sztuczna zastawka"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(11, diseases[5], "brak"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(12, diseases[5], "łagodna"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(13, diseases[5], "umiarkowana"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(14, diseases[5], "ciężka"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(15, diseases[5], "sztuczna zastawka"));
         // CVE przebyty
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(14, diseases[5], "nie"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(15, diseases[5], "tak"));
-        // CHF
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(16, diseases[6], "nie"));
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(17, diseases[6], "tak"));
-        // DM
+        // CHF
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(18, diseases[7], "nie"));
         diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(19, diseases[7], "tak"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(20, diseases[7], "w trakcie insulinoterapii"));
+        // DM
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(20, diseases[8], "nie"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(21, diseases[8], "tak"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(22, diseases[8], "w trakcie insulinoterapii"));
         // COPD
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(21, diseases[8], "nie"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(22, diseases[8], "tak"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(23, diseases[9], "nie"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(24, diseases[9], "tak"));
         // EKG
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(23, diseases[9], "rytm zatokowy"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(24, diseases[9], "AF"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(25, diseases[9], "rytm zatokowy + obecność VE"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(26, diseases[9], "AF + obecność VE"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(27, diseases[9], "rytm z rozrusznika"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(28, diseases[9], "AF + rytm z rozrusznika"));
-		//wstrzas
-		diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(29, diseases[10], "nie"));
-        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(30, diseases[10], "tak"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(25, diseases[10], "rytm zatokowy"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(26, diseases[10], "AF"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(27, diseases[10], "rytm zatokowy + obecność VE"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(28, diseases[10], "AF + obecność VE"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(29, diseases[10], "rytm z rozrusznika"));
+        diseaseDescriptionService.saveOrUpdate(new DiseaseDescription(30, diseases[10], "AF + rytm z rozrusznika"));
 
 
         // badania_s
