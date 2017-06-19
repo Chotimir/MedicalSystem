971951d4be00bce7895b184352f85ecb0bf25e82
tradwan <tomasz.x.radwan@gmail.com>
2017-06-19 16:00:40 +0200

    Fix for services (@Injectable in parent form service), mock data for comorbidities testing

--- frontend/src/app/services/comorbidities.service.ts
+++ frontend/src/app/services/comorbidities.service.ts
@@ -4,17 +4,26 @@ import 'rxjs/add/operator/toPromise';
 import {SelectField} from "../model/select-field";
 import {FormService} from "./form-service.service";
 
+const mockdata: SelectField[] = [
+  { name: "choroba 1", values: ["tak", "nie", "nwm"], selected: null},
+  { name: "choroba 2", values: ["tak", "nie", "nwm"], selected: null },
+  { name: "choroba 3", values: ["tak", "nie", "nwm"], selected: "tak" },
+  { name: "choroba 4", values: ["tak", "nie", "nwm"], selected: "nie" },
+  { name: "choroba 5", values: ["tak", "nie", "nwm"], selected: "nwm" }
+];
+
 @Injectable()
 export class ComorbiditiesService extends FormService<SelectField[]> {
 
   private comorbiditiesUrl = 'api/comorbidities';
 
   getComorbidities(): Promise<SelectField[]> {
-    return this.get(this.comorbiditiesUrl);
+    return Promise.resolve(mockdata);
+    // return this.get(this.comorbiditiesUrl);
   }
 
   updateComorbidities(comorbidities: SelectField[]): void {
-    this.update(comorbidities, this.comorbiditiesUrl);
+    // this.update(comorbidities, this.comorbiditiesUrl);
   }
 
 }
--- frontend/src/app/services/form-service.service.ts
+++ frontend/src/app/services/form-service.service.ts
@@ -1,5 +1,7 @@
 import {Headers, Http} from "@angular/http";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export abstract class FormService<T> {
 
   private headers = new Headers({'Content-Type': 'application/json'});
