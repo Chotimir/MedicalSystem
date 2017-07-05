4396a8c7c21a609dd5b70cb493fb92fe287d1834
Jakub Piekarz <jpiekarz12@outlook.com>
2017-07-06 00:16:09 +0200

    Fix importing diseases

--- backend/src/main/java/com/medicalsystem/excel/builder/DiseaseBuilder.java
+++ backend/src/main/java/com/medicalsystem/excel/builder/DiseaseBuilder.java
@@ -1,21 +1,56 @@
 package com.medicalsystem.excel.builder;
 
+import com.medicalsystem.excel.CellValue;
+import com.medicalsystem.excel.ExcelColumnsProperties;
 import com.medicalsystem.model.Disease;
+import com.medicalsystem.model.DiseaseDescription;
+import com.medicalsystem.service.DiseaseDescriptionService;
+import com.medicalsystem.service.DiseaseService;
+import lombok.AllArgsConstructor;
 import org.apache.poi.ss.usermodel.Row;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class DiseaseBuilder {
 
-    // TODO: problem z List<DiseaseDescription> w Disease
-    // TODO: (powinna być jedna wartość DiseaseDescription zamiast listy) ~MS
-    public List<Disease> build(Row row) {
-        List<Disease> diseases = new ArrayList<>();
+    private final ExcelColumnsProperties columnsProperties;
+    private final DiseaseService diseaseService;
+    private final DiseaseDescriptionService diseaseDescriptionService;
 
-        return diseases;
+    public List<DiseaseDescription> build(Row row) {
+        List<DiseaseDescription> diseaseDescriptions = new ArrayList<>();
+
+        /* Get index of the column of the first disease */
+        int firstExamIndex = columnsProperties.getPropertyAsInt("disease.shock.number");
+
+        /* Get index of the column of the last disease */
+        int lastExamIndex = columnsProperties.getPropertyAsInt("disease.ekg.number");
+
+        /* Iterate over diseases - assumes that disease description ids are in proper order */
+        int descriptionId = 1;
+
+        for (int i = firstExamIndex; i <= lastExamIndex; i++, descriptionId++) {
+
+            /* Get disease result */
+            CellValue resultCell = new CellValue(row, i);
+            int result = resultCell.getAsInt();
+
+            if (result > 0) {
+                Disease disease = diseaseService.getById(descriptionId);
+                DiseaseDescription diseaseDescription = diseaseDescriptionService.getByDiseaseAndExcelValue(disease, result);
+
+                if(diseaseDescription != null){
+                    diseaseDescriptions.add(diseaseDescription);
+                }
+            }
+
+        }
+
+        return diseaseDescriptions;
     }
-
 }
--- backend/src/main/java/com/medicalsystem/service/DiseaseDescriptionService.java
+++ backend/src/main/java/com/medicalsystem/service/DiseaseDescriptionService.java
@@ -1,6 +1,8 @@
 package com.medicalsystem.service;
 
+import com.medicalsystem.model.Disease;
 import com.medicalsystem.model.DiseaseDescription;
 
 public interface DiseaseDescriptionService extends CRUDService<DiseaseDescription> {
+    DiseaseDescription getByDiseaseAndExcelValue(Disease disease, Integer excelValue);
 }
