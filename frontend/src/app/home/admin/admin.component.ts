79ac17ea4c18d75da9c1dd98f1039b7d49a19168
tradwan <tomasz.x.radwan@gmail.com>
2017-07-17 22:10:56 +0200

    Add user table to admin panel

--- frontend/src/app/home/admin/admin.component.ts
+++ frontend/src/app/home/admin/admin.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from "../../services/users.service";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [UsersService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  private users: User[];
+
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
   }
