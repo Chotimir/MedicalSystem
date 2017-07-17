2bea42e349d8ee7e767baf4d07d579a95b35ae45
kamildev7 <k.93kamil@gmail.com>
2017-07-17 19:40:33 +0200

    change ExportController

--- backend/src/main/java/com/medicalsystem/controller/ExportController.java
+++ backend/src/main/java/com/medicalsystem/controller/ExportController.java
@@ -1,6 +1,9 @@
 package com.medicalsystem.controller;
 
+import com.medicalsystem.excel.export.ExcelExporter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +20,23 @@ public class ExportController {
      * Temporary method
      * @return hard-coded excel file
      */
-    @GetMapping("api/export")
-    public ResponseEntity<?> exportToExcel() throws IOException {
-        File file = new File("baza2.xlsx");
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+//    @GetMapping("api/export")
+//    public ResponseEntity<?> exportToExcel() throws IOException {
+//        File file = new File("baza2.xlsx");
+//        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+//
+//        return ResponseEntity.ok()
+//                .contentLength(file.length())
+//                .contentType(MediaType.parseMediaType("application/octet-stream"))
+//                .body(resource);
+//    }
 
-        return ResponseEntity.ok()
-                .contentLength(file.length())
-                .contentType(MediaType.parseMediaType("application/octet-stream"))
-                .body(resource);
+    @Autowired
+    private ExcelExporter excelExporter;
+
+    @GetMapping("/export")
+    public ResponseEntity<?> exportToExcel() {
+        excelExporter.exportToExcel();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
